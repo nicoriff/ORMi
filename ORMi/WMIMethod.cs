05b2df53f8fd4930b251ab89d65020800a775515
git <nicolas.luque@prosegur.com>
2018-09-17 15:17:43 -0300

    Add support for generic types on WmiMethod class. Improvement made for issue #4

--- ORMi/WMIMethod.cs
+++ ORMi/WMIMethod.cs
@@ -11,10 +11,13 @@ using System.Threading.Tasks;
 
 namespace ORMi
 {
+    /// <summary>
+    /// Static class for WMI method helpers.
+    /// </summary>
     public static class WMIMethod
     {
         /// <summary>
-        /// Executes an WMI instance method with no parameter.
+        /// Executes an WMI instance method with no parameter. Returns dynamic object.
         /// </summary>
         /// <param name="obj">Instance which will be instanciated to call the method.</param>
         /// <returns></returns>
@@ -35,7 +38,29 @@ namespace ORMi
         }
 
         /// <summary>
-        /// Executes an instance method with parameters. 
+        /// Executes WMI instance method with no parameter. Returns an object of specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">Instance which will be instanciated to call the method.</param>
+        /// <returns></returns>
+        public static T ExecuteMethod<T>(object obj)
+        {
+            WindowsImpersonationContext impersonatedUser = WindowsIdentity.GetCurrent().Impersonate();
+
+            var mth = new StackTrace().GetFrame(1).GetMethod();
+            string methodName = mth.Name;
+
+            ManagementClass genericClass = new ManagementClass(TypeHelper.GetNamespace(obj), TypeHelper.GetClassName(obj), null);
+
+            ManagementObject instance = TypeHelper.GetManagementObject(genericClass, obj);
+
+            ManagementBaseObject result = instance.InvokeMethod(methodName, null, null);
+
+            return (T)TypeHelper.LoadObject(result, typeof(T));
+        }
+
+        /// <summary>
+        /// Executes an instance method with parameters. Returns a dynamic object.
         /// </summary>
         /// <param name="obj">Instance which will be instanciated to call the method.</param>
         /// <param name="parameters">Anonymous object with properties matching the parameter names of the method.</param>
@@ -65,6 +90,37 @@ namespace ORMi
         }
 
         /// <summary>
+        /// Executes an instance method with parameters. Returns an object of specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static T ExecuteMethod<T>(object obj, dynamic parameters)
+        {
+            WindowsImpersonationContext impersonatedUser = WindowsIdentity.GetCurrent().Impersonate();
+
+            var frame = new StackTrace().GetFrames().Skip(1).First(x => x.GetMethod().DeclaringType.Namespace != "System.Dynamic");
+
+            string methodName = frame.GetMethod().Name;
+
+            ManagementClass genericClass = new ManagementClass(TypeHelper.GetNamespace(obj), TypeHelper.GetClassName(obj), null);
+
+            ManagementObject instance = TypeHelper.GetManagementObject(genericClass, obj);
+
+            ManagementBaseObject inParams = genericClass.GetMethodParameters(methodName);
+
+            foreach (PropertyInfo p in parameters.GetType().GetProperties())
+            {
+                inParams[p.Name] = p.GetValue(parameters);
+            }
+
+            ManagementBaseObject result = instance.InvokeMethod(methodName, inParams, null);
+
+            return (T)TypeHelper.LoadObject(result, typeof(T));
+        }
+
+        /// <summary>
         /// Executes a static method without parameters.
         /// </summary>
         /// <returns></returns>
@@ -80,6 +136,25 @@ namespace ORMi
             ManagementBaseObject result = cls.InvokeMethod(methodName, null, null);
 
             return TypeHelper.LoadDynamicObject(result);
+        }
+
+        /// <summary>
+        /// Executes a static method without parameters. Returns an object of specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T ExecuteStaticMethod<T>()
+        {
+            var mth = new StackTrace().GetFrame(1).GetMethod();
+            string methodName = mth.Name;
+
+            Type t = mth.ReflectedType;
+
+            ManagementClass cls = new ManagementClass(TypeHelper.GetNamespace(t), TypeHelper.GetClassName(t), null);
+
+            ManagementBaseObject result = cls.InvokeMethod(methodName, null, null);
+
+            return (T)TypeHelper.LoadObject(result, typeof(T));
         }
 
         /// <summary>
@@ -107,6 +182,34 @@ namespace ORMi
             ManagementBaseObject result = cls.InvokeMethod(methodName, inParams, null);
 
             return TypeHelper.LoadDynamicObject(result);
+        }
+
+        /// <summary>
+        /// Executes a static method with parameters. Returns an object of specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static T ExecuteStaticMethod<T>(dynamic parameters)
+        {
+            var frame = new StackTrace().GetFrames().Skip(2).First(x => x.GetMethod().DeclaringType.Namespace != "System.Dynamic");
+
+            string methodName = frame.GetMethod().Name;
+
+            Type t = frame.GetMethod().ReflectedType;
+
+            ManagementClass cls = new ManagementClass(TypeHelper.GetNamespace(t), TypeHelper.GetClassName(t), null);
+
+            ManagementBaseObject inParams = cls.GetMethodParameters(methodName);
+
+            foreach (PropertyInfo p in parameters.GetType().GetProperties())
+            {
+                inParams[p.Name] = p.GetValue(parameters);
+            }
+
+            ManagementBaseObject result = cls.InvokeMethod(methodName, inParams, null);
+
+            return (T)TypeHelper.LoadObject(result, typeof(T));
         }
     }
 }
